abf3b68a84a8dabddc188dd9026122163dc32e84
MichaelTheTall <michaeljhnewman@gmail.com>
2018-08-07 09:11:41 +0100

    Tidied up library code

--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -5,89 +5,28 @@ require('../library.rb')
 class LibraryTest < MiniTest::Test
 
 def setup
-  @lib = [{:title=>"lord_of_the_rings", :rental_details=>{:student_name=>"Jeff", :date=>"01/12/16"}}, {:title=>"i_robot", :rental_details=>{:student_name=>"Bob", :date=>"24/06/16"}}]
+  @lib = Library.new([{:title=>"lord_of_the_rings", :rental_details=>{:student_name=>"Jeff", :date=>"01/12/16"}}, {:title=>"i_robot", :rental_details=>{:student_name=>"Bob", :date=>"24/06/16"}}])
 end
 
 def test_getter
-  library = Library.new(
-    [{
-     title: "lord_of_the_rings",
-     rental_details: {
-      student_name: "Jeff",
-      date: "01/12/16"
-     }
-    },
-    {
-      title: "i_robot",
-      rental_details: {
-       student_name: "Bob",
-       date: "24/06/16"
-      }
-    }])
-  assert_equal([{:title=>"lord_of_the_rings", :rental_details=>{:student_name=>"Jeff", :date=>"01/12/16"}}, {:title=>"i_robot", :rental_details=>{:student_name=>"Bob", :date=>"24/06/16"}}], library.books)
+  assert_equal([{:title=>"lord_of_the_rings", :rental_details=>{:student_name=>"Jeff", :date=>"01/12/16"}}, {:title=>"i_robot", :rental_details=>{:student_name=>"Bob", :date=>"24/06/16"}}], @lib.books)
 end
 
 def test_get_book
-  library = Library.new(
-    [{
-     title: "lord_of_the_rings",
-     rental_details: {
-      student_name: "Jeff",
-      date: "01/12/16"
-     }
-    },
-    {
-      title: "i_robot",
-      rental_details: {
-       student_name: "Bob",
-       date: "24/06/16"
-      }
-    }])
   assert_equal({
     title: "i_robot",
     rental_details: {
      student_name: "Bob",
      date: "24/06/16"
     }
-  }, library.get_book("i_robot"))
+  }, @lib.get_book("i_robot"))
 end
 
 def test_get_rental_info
-  library = Library.new(
-    [{
-     title: "lord_of_the_rings",
-     rental_details: {
-      student_name: "Jeff",
-      date: "01/12/16"
-     }
-    },
-    {
-      title: "i_robot",
-      rental_details: {
-       student_name: "Bob",
-       date: "24/06/16"
-      }
-    }])
-    assert_equal({:student_name=>"Bob", :date=>"24/06/16"}, library.get_rental_info("i_robot"))
+    assert_equal({:student_name=>"Bob", :date=>"24/06/16"}, @lib.get_rental_info("i_robot"))
   end
 
   def test_add_book
-    library = Library.new(
-      [{
-       title: "lord_of_the_rings",
-       rental_details: {
-        student_name: "Jeff",
-        date: "01/12/16"
-       }
-      },
-      {
-        title: "i_robot",
-        rental_details: {
-         student_name: "Bob",
-         date: "24/06/16"
-        }
-      }])
-
       new_book = {
         title: "starship_troopers",
         rental_details: {
@@ -96,27 +35,12 @@ def test_get_rental_info
         }
       }
 
-      assert_equal([{:title=>"lord_of_the_rings", :rental_details=>{:student_name=>"Jeff", :date=>"01/12/16"}}, {:title=>"i_robot", :rental_details=>{:student_name=>"Bob", :date=>"24/06/16"}}, {:title=>"starship_troopers", :rental_details=>{:student_name=>"", :date=>""}}], library.add_book(new_book))
+      assert_equal([{:title=>"lord_of_the_rings", :rental_details=>{:student_name=>"Jeff", :date=>"01/12/16"}}, {:title=>"i_robot", :rental_details=>{:student_name=>"Bob", :date=>"24/06/16"}}, {:title=>"starship_troopers", :rental_details=>{:student_name=>"", :date=>""}}], @lib.add_book(new_book))
   end
 
   def test_change_rental
-    library = Library.new(
-      [{
-       title: "lord_of_the_rings",
-       rental_details: {
-        student_name: "Jeff",
-        date: "01/12/16"
-       }
-      },
-      {
-        title: "i_robot",
-        rental_details: {
-         student_name: "Bob",
-         date: "24/06/16"
-        }
-      }])
-      library.change_rental("i_robot", "Joe", "05/07/16")
-    assert_equal({:title=>"i_robot", :rental_details=>{:student_name=>"Joe", :date=>"05/07/16"}}, library.get_book("i_robot"))
+    @lib.change_rental("i_robot", "Joe", "05/07/16")
+    assert_equal({:title=>"i_robot", :rental_details=>{:student_name=>"Joe", :date=>"05/07/16"}}, @lib.get_book("i_robot"))
   end
 
 end
